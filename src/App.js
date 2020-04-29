65832235ecb3bf57eb72fe734935d144f4a1ec33
bobissict <bo@goturtle.ai>
2020-04-29 11:26:03 +0200

    implemented suspense and error boundaries

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import About from './pages/About';
+import ErrorBoundary from './components/ErrorBoundary';
+
+const About = lazy(() => import('./pages/About'));
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Route path="/about">
+                <About />
+              </Route>
+            </Suspense>
+          </Switch>
+        </div>
+      </Router>
+    </ErrorBoundary>
   );
 }
